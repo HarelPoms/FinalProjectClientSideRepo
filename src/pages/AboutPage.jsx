4e496e714e466035de33982b4aa92d8246737af4
HarelPoms <95750678+HarelPoms@users.noreply.github.com>
2023-09-12 16:14:22 +0300

    about styling edit

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -20,7 +20,7 @@ const AboutPage = () => {
             <Grid container spacing={2}>
                 {/* Row one */}
                 <Grid item xs={5} sm={5} sx={{mt:5, ml:1}}>
-                    <Typography sx={{bgcolor:"limegreen"}}>This is a medicine card, including an image, brief subtitle and description.beneath it, is a row containing the myriad of actions your user may perform.
+                    <Typography className="aboutTypography">This is a medicine card, including an image, brief subtitle and description.beneath it, is a row containing the myriad of actions your user may perform.
                     The Edit button allows you to edit the medicine card only if the card was created by you as a pharma user or admin.
                     The delete button is available if you're either an Admin, or you created the card as a pharma user. 
                     </Typography>
@@ -30,7 +30,7 @@ const AboutPage = () => {
                 </Grid>
                 {/* Row Two */}
                 <Grid item xs={5} sm={5} sx={{mt:5, ml:1}}>
-                    <Typography sx={{bgcolor:"limegreen"}}>
+                    <Typography className="aboutTypography">
                         You may also perform like/unlike via the button available if you are a patient. You can add any medicine to the shopping Cart via the cart button.
                     </Typography>
                 </Grid>
@@ -39,7 +39,7 @@ const AboutPage = () => {
                 </Grid>
                 {/* Row Three */}
                 <Grid item xs={5} sm={5} sx={{ml:1}}>
-                    <Typography sx={{bgcolor:"limegreen"}}>
+                    <Typography className="aboutTypography">
                         This is a prescription card, the actions available to you depend on your role (delete, edit, approve [for doctors])
                     </Typography>
                 </Grid>
@@ -48,7 +48,7 @@ const AboutPage = () => {
                 </Grid>
                 {/* Row Four */}
                 <Grid item xs={5} sm={5} sx={{ml:1}}>
-                    <Typography sx={{bgcolor:"limegreen"}}>
+                    <Typography className="aboutTypography">
                         This is a prescription card as it appears under unassigned prescriptions for doctors, this page will only show prescriptions that have not had a doctor assume responsibility for them for patients making prescription requests of the same HMO as the doctor. A doctor from the same HMO may assume responsibility over it by pushing the button shown.
                     </Typography>
                 </Grid>
@@ -57,7 +57,7 @@ const AboutPage = () => {
                 </Grid>
                 {/* Row Five */}
                 <Grid item xs={5} sm={5} sx={{ml:1}}>
-                    <Typography sx={{bgcolor:"limegreen"}}>
+                    <Typography className="aboutTypography">
                         This is right side of the navbar, here you may switch between light mode and dark mode, as well as search for particular items by medicine number or by name.
                     </Typography>
                 </Grid>
@@ -66,7 +66,7 @@ const AboutPage = () => {
                 </Grid>
                 {/* Row Six */}
                 <Grid item xs={5} sm={5} sx={{ml:1}}>
-                    <Typography sx={{bgcolor:"limegreen"}}>
+                    <Typography className="aboutTypography">
                         This is left side of the navbar, here you may perform any number of actions or access pages available to you on the basis of your user's permissions
                     </Typography>
                 </Grid>
@@ -75,7 +75,7 @@ const AboutPage = () => {
                 </Grid>
                 {/* Row Seven */}
                 <Grid item xs={5} sm={5} sx={{ml:1}}>
-                    <Typography sx={{bgcolor:"limegreen"}}>
+                    <Typography className="aboutTypography">
                         This is the footer; <br /> My favorite medicines is available to patients users.
                         <br />My Medicines is available to pharma users.<br /> About is available to anyone.
                     </Typography>
