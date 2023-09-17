bd85ba69b58c9e06b5a7eba3ec3a6a0e3633d3f9
HarelPoms <95750678+HarelPoms@users.noreply.github.com>
2023-09-17 15:29:17 +0300

    tweaks

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -16,13 +16,10 @@ const ContactUsPage = () => {
         <Box component="div" noValidate sx={{ mt: 3 }}>
             <Typography variant="h2" className="alignTextToCenter">How to contact us:</Typography>
             <Grid container spacing={2}>
-                {/* Row one */}
                 <Grid item xs={5} sm={5} sx={{mt:5, ml:1}}>
-                    <Typography className="aboutTypography">
-                        Telephone : 03-5461232
-                        Fax : +44 161 999 8888
-                        Address : 221B Baker St., London
-                    </Typography>
+                    <Typography className="aboutTypography">Telephone : 03-5461232</Typography>
+                    <Typography className="aboutTypography">Fax : +44 161 999 8888</Typography>
+                    <Typography className="aboutTypography">Address : 221B Baker St., London</Typography>
                 </Grid>
                 <Grid item xs={6} sm={6} md={6} >
                     <Img alt="contact us image" src={"/assets/images/telephone.jpg"} />
